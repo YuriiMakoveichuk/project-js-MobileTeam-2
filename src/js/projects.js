6db31ead85d160946f54da3765da9e366e1dc99a
OksanaLynnyk <119429489+OksanaLynnyk@users.noreply.github.com>
2024-07-11 00:01:14 +0300

    img fixed

--- src/js/projects.js
+++ src/js/projects.js
@@ -4,7 +4,7 @@ import 'swiper/css/bundle';
 new Swiper('.swiper',{
     centeredSlides: true,
     centeredSlidesBounds: true,
-    spaceBetween: 20,
+    spaceBetween: 30,
     navigation: {
         nextEl: '.swiper-button-next.projects-btn',
         prevEl: '.swiper-button-prev.projects-btn',
